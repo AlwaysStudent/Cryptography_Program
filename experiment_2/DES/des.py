11854358d47edd8150960fd68b2f84f039e56fad
AlwaysStudent <whetherstudent@163.com>
2019-05-26 14:45:13 +0800

    update experiment2 but it still can't work, there is some mistake in it and I will update it recently

--- experiment_2/DES/des.py
+++ experiment_2/DES/des.py
@@ -37,6 +37,7 @@ class des:
         self.plain_text = plain_text
 
 
+
         return self.crypto_text
 
     def decode(self, crypto_text):
@@ -94,6 +95,53 @@ class des:
                 key_temp += ''.join([str(k) for k in temp])
                 temp = []
         self.key = bin2hex(key_temp)
+
+    def run_coding(self, text, number):
+        temp_text = ascii2bin(text)
+        atfer_ip_switch_text = ip_switch(temp_text, 1)
+        atfer_feistel_text = feistel(atfer_ip_switch_text, self.key_list, number)
+
+
+def feistel(text, key_list, number):
+    temp_text = text
+    for i in range(16):
+        letf_text = temp_text[:32]
+        right_text = temp_text[32:]
+        temp =
+        temp_text = right_text +
+
+
+def ascii2bin(text):
+
+
+
+def ip_switch(text, ip_choose):
+    ip_switch_matrix =  [[58, 50, 42, 34, 26, 18, 10, 2],
+                        [60, 52, 44, 36, 28, 20, 12, 4],
+                        [62, 54, 46, 38, 30, 22, 14, 6],
+                        [64, 56, 48, 40, 32, 24, 16, 8],
+                        [57, 49, 41, 33, 25, 17, 9 , 1],
+                        [59, 51, 43, 35, 27, 19, 11, 3],
+                        [61, 53, 45, 37, 29, 21, 13, 5],
+                        [63, 55, 47, 39, 31, 23, 15, 7]]
+    ip_switch_matrix_1 = [[40, 8, 48, 16, 56, 24, 64, 32],
+                          [39, 7, 47, 15, 55, 23, 63, 31],
+                          [38, 6, 46, 14, 54, 22, 62, 30],
+                          [37, 5, 45, 13, 53, 21, 61, 29],
+                          [36, 4, 44, 12, 52, 20, 60, 28],
+                          [35, 3, 43, 11, 51, 19, 59, 27],
+                          [34, 2, 42, 10, 50, 18, 58, 26],
+                          [33, 1, 41,  9, 49, 17, 57, 25]]
+    result_text = ''
+    if ip_choose == 1:
+        result_text = switch_by_matrix(text, ip_switch_matrix)
+    if ip_choose == -1:
+        result_text = switch_by_matrix(text, ip_switch_matrix_1)
+    return result_text
+
+
+def xor(a, b):
+    return a ^ b
 
 
 def add_zero(source_text, number):
@@ -154,7 +202,7 @@ def add_zero_last(source_text, number):
 
 def test():
     # test des
-    key1 = 'f'*14
+    key1 = 'e'*14
     x = des(key1)
     print('\n\n')
     print(x.real_key)
